18c00e23bd832f1a37f29b07965645e615e9949d
Joel Crawford <macbook@Crawford.local>
2023-03-05 18:25:32 +0300

    Create User

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -61,19 +61,19 @@ class UserController extends Controller
 
 
 
-    public function findUserData()
-    {
-        if ($search = \Request::get('q')) {
-            $users = User::where(function($query) use ($search){
-                $query->where('name','LIKE',"%$search%")
-                        ->orWhere('email','LIKE',"%$search%");
-            })->paginate(20);
-        }else{
-            $users = User::latest()->paginate(5);
-        }
+    // public function findUserData()
+    // {
+    //     if ($search = \Request::get('q')) {
+    //         $users = User::where(function($query) use ($search){
+    //             $query->where('name','LIKE',"%$search%")
+    //                     ->orWhere('email','LIKE',"%$search%");
+    //         })->paginate(20);
+    //     }else{
+    //         $users = User::latest()->paginate(5);
+    //     }
 
-        return apiResponse($users);
-    }
+    //     return apiResponse($users);
+    // }
 
 
 }
