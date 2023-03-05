20bbe30c7b9dac092614b1565769d6147160944e
Joel Crawford <macbook@Crawford.local>
2023-03-05 18:40:01 +0300

    Refactored Code

--- app/Http/Middleware/JsonMiddleware.php
+++ app/Http/Middleware/JsonMiddleware.php
@@ -16,6 +16,8 @@ class JsonMiddleware
     public function handle($request, Closure $next)
     {
         $request->headers->set('Accept', 'application/json');
-        return $next($request);
+        return $next($request)->header("Access-Control-Allow-Origin", "*")
+            ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+            ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
     }
 }
