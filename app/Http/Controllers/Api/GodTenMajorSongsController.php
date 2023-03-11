f8189a5b260eb00485c3649de764b806f652b5e1
Joel Crawford <macbook@Crawford.local>
2023-03-11 20:00:48 +0300

    Request classes for DVDs and Songs

---
+++ app/Http/Controllers/Api/GodTenMajorSongsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class GodTenMajorSongsController extends Controller
+{
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+
+
+}
---
+++ app/Http/Controllers/Api/VideoDVDController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class VideoDVDController extends Controller
+{
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+
+}
