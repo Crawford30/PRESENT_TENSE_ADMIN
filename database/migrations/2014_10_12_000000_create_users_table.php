d92757a170d7789cdaa36beebff497ae28b1d403
Joel Crawford <macbook@Crawford.local>
2023-03-11 18:00:18 +0300

    refactored migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('type')->default('user');
-            $table->enum('user_status', ['ACTIVATED', 'DEACTIVATED', 'PENDING'])->default('PENDING');
+            $table->enum('user_status', ['ACTIVATED', 'DEACTIVATED', 'PENDING'])->default('DEACTIVATED');
             $table->enum('dvd_access_status', ['NONE', 'AUDIO_DVD', 'VIDEO_DVD', 'ALL'])->default('NONE');
             $table->enum('song_access_status', ['NO',  'YES'])->default('NO');
             $table->enum('is_email_verified', ['NO', 'YES'])->default('NO');
