32d84741c714036d2a28176bb7352d26c7795b42
Joel Crawford <joelcrawford30@gmail.com>
2023-06-19 22:25:16 +0300

    Fixed::Query for not giving proper user data

--- app/Http/Requests/VideoDVDRequest/GetUserVideoDVDListRequest.php
+++ app/Http/Requests/VideoDVDRequest/GetUserVideoDVDListRequest.php
@@ -35,22 +35,51 @@ class GetUserVideoDVDListRequest extends FormRequest
     public function getUserVideoDVDList() {
 
 
-        $user =   User::where('id',auth()->user()->id)
-        ->where('user_status', 'ACTIVATED')
-        ->where('dvd_access_status', 'VIDEO_DVD')
-        ->orWhere('dvd_access_status','ALL')
-        ->whereNotNull('video_dvd_permission')
-        ->where('type', 'user')
-        ->orWhere('type', 'admin')->first();
+     $user = User::where('id', auth()->user()->id)
+    ->where('user_status', 'ACTIVATED')
+    ->where('dvd_access_status', 'VIDEO_DVD')
+    ->orWhere('dvd_access_status', 'ALL')
+    ->whereNotNull('video_dvd_permission')
+    ->where(function ($query) {
+        $query->where('type', 'user')
+            ->orWhere('type', 'admin');
+    })
+    ->first();
 
+if ($user) {
+    $userVideoLists = UserVideoDVDPermission::where("permission", $user->video_dvd_permission)
+        ->pluck('video_d_v_d_id')
+        ->toArray();
 
-        $userAudioLists = UserVideoDVDPermission::where("permission", $user->video_dvd_permission)->pluck('video_d_v_d_id')->toArray();
-        $itemCollection = DB::table('video_d_v_d_s')
-        ->whereIn('id', $userAudioLists)
+    $itemCollection = DB::table('video_d_v_d_s')
+        ->whereIn('id', $userVideoLists)
         ->get();
 
+    return apiResponse($itemCollection);
+} else {
 
-        return apiResponse($itemCollection);
+    return apiResponse("No video found");
+    // Handle the case when $user is null
+    // You can set appropriate default values or throw an exception
+}
+
+
+        // $user =   User::where('id',auth()->user()->id)
+        // ->where('user_status', 'ACTIVATED')
+        // ->where('dvd_access_status', 'VIDEO_DVD')
+        // ->orWhere('dvd_access_status','ALL')
+        // ->whereNotNull('video_dvd_permission')
+        // ->where('type', 'user')
+        // ->orWhere('type', 'admin')->first();
+
+
+        // $userAudioLists = UserVideoDVDPermission::where("permission", $user->video_dvd_permission)->pluck('video_d_v_d_id')->toArray();
+        // $itemCollection = DB::table('video_d_v_d_s')
+        // ->whereIn('id', $userAudioLists)
+        // ->get();
+
+
+        // return apiResponse($itemCollection);
     }
 
 }
