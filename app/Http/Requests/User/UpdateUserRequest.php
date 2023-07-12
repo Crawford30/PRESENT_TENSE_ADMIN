bcaff2d2efef1210ff4284c26ad6cc45e911eb20
Joel Crawford <joelcrawford30@gmail.com>
2023-07-12 14:13:28 +0300

    Update User record

--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -33,55 +33,38 @@ class UpdateUserRequest extends FormRequest
 
     public function updateeUser($request){
 
-
         $user = User::findOrFail($request->id);
 
-       // dd($user);
-
-
-        // $this->Validator::make($request, [
-        //     "name" => "required|string|max:191",
-        //     "email" => 'required|string|email|max:191|unique:users,email,'.$user->id,
-        //     "password" => "sometimes|min:6",
-        // ]);
-
-
-        $request->validate([
-        "name" => "required|string|max:191",
-        "email" => 'required|string|email|max:191|unique:users,email,'.$user->id,
-        "password" => "sometimes|min:6"]);
-
-
-
-        // name: "",
-        // email: "",
-        // songPermission: "",
-        // audioDVDPermission: "",
-        // videoDVDPermission: "",
-        // password: "",
-        // type: "",
-
-        $data = [
-            "name" => $request->name,
-            "email" => $request->email,
-            "type" => $request->type,
-            "user_status" => $request->user_status,
-            "dvd_access_status" => $request->dvd_access_status,
-            "audio_dvd_permission" => $request->audio_dvd_permission,
-            "video_dvd_permission" => $request->video_dvd_permission,
-            "password" => Hash::make($request->password),
-
+        $rules = [
+            'name' => 'required|string|max:191',
+            'email' => 'required|string|email|max:191|unique:users,email,'.$user->id,
         ];
 
+        // Add the password rule only when it is provided in the request
+        if ($request->filled('password')) {
+            $rules['password'] = 'sometimes|min:6';
+        }
 
-        $userData = user::updateOrCreate([
-            'id' => $request->id], $data);
-          return apiResponse($userData);
+        $request->validate($rules);
 
-        // $user->update($request->all());
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'type' => $request->type,
+            'user_status' =>  $request->user_status,
+            'dvd_access_status' => $request->dvd_access_status,
+            'audio_dvd_permission' => $request->audio_dvd_permission,
+            'video_dvd_permission' => $request->video_dvd_permission,
+        ];
 
-        // return apiResponse($user);
+        // Update the password only when it is provided in the request
+        if ($request->filled('password')) {
+            $data['password'] = Hash::make($request->password);
+        }
 
+        $userData = User::updateOrCreate(['id' => $request->id], $data);
+
+        return apiResponse($userData);
 
     }
 }
